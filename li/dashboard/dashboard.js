b1c73e193a433d801d7e3ad8c7c6da2c1cc655e2
resu <resu062@gmail.com>
2021-07-20 11:30:21 +0300

    dashboard

--- li/dashboard/dashboard.js
+++ li/dashboard/dashboard.js
@@ -1,4 +1,4 @@
-import { LiElement, html, css } from '../../li.js';
+import { LiElement, html, css, unsafeCSS } from '../../li.js';
 
 import '../layout-app/layout-app.js';
 import '../button/button.js';
@@ -17,18 +17,10 @@ customElements.define('li-dashboard', class LiDashboard extends LiElement {
             ::-webkit-scrollbar-thumb {
                 background-color: gray;
             }
-            #app-main {
-                display: flex;
-                height: calc(100% - 10px);
-                margin: 4px 4px 4px 0;
-                /* border: 1px solid lightgray; */
-                align-items: flex-start;
-                flex-wrap: wrap;
-            }
             .color{
                 border: 1px solid lightgray; 
-                width: calc(100% - 6px); 
-                height:32px; 
+                width: (100% - 6px); 
+                height: 32px; 
                 margin: 2px;
                 cursor: pointer;
             }
@@ -47,40 +39,87 @@ customElements.define('li-dashboard', class LiDashboard extends LiElement {
                             @dblclick="${e => this._dblclick(e, i)}"></div>
                     `)}
                 </div>
-                <div slot="app-main" id="app-main" @dragover="${this._over}" @drop="${this._drop}">
-                    ${(this.items || []).map(i => html`
-                        <li-dashpanel .item="${i}"></li-dashpanel>   
-                    `)}
-                </div>
+                <li-dashpanel slot="app-main" .item="${this.item}"></li-dashpanel>   
             </li-layout-app>
         `;
     }
 
     static get properties() {
         return {
-            items: { type: Array, default: [] },
+            item: {
+                type: Object,
+                default: {
+                    vertical: false,
+                    items: [
+                        { color: `hsla(0, 50%, 70%, .7)`, w: '400px', h: '99%' },
+                        {
+                            vertical: true,
+                            items: [
+                                { color: `hsla(45, 50%, 70%, .7)`, h: '400px' },
+                                {
+                                    vertical: false,
+                                    items: [
+                                        { color: `hsla(90, 50%, 70%, .7)` },
+                                        {
+                                            vertical: true,
+                                            items: [
+                                                { color: `hsla(135, 50%, 70%, .7)` },
+                                                {
+                                                    vertical: true,
+                                                    items: [
+                                                        { color: `hsla(180, 50%, 70%, .7)` },
+                                                        {
+                                                            vertical: false,
+                                                            items: [
+                                                                { color: `hsla(225, 50%, 70%, .7)` },
+                                                                {
+                                                                    vertical: false,
+                                                                    items: [
+                                                                        { color: `hsla(270, 50%, 70%, .7)` },
+                                                                        {
+                                                                            vertical: true,
+                                                                            items: [
+                                                                                { color: `hsla(315, 50%, 70%, .7)` },
+                                                                                { color: `hsla(360, 50%, 70%, .7)` }
+                                                                            ]
+                                                                        }
+                                                                    ]
+                                                                }
+                                                            ]
+                                                        }
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            },
             focusedItem: { type: Object, default: undefined, local: true },
-            readOnly: { type: Boolean, reflect: true, local: true}
+            readOnly: { type: Boolean, reflect: true }
         }
     }
 
     _start(e, color) {
         //console.log(color)
-        this._dragElementColor = color;
+        //this._dragElementColor = color;
     }
     _over(e) {
         //console.log(e)
-        e.preventDefault();
+        //e.preventDefault();
         //e.dataTransfer.dropEffect = "move";
     }
     _drop(e) {
-        this.items = this.items || [];
-        this.items.push({ color: `hsla(${this._dragElementColor}, 50%, 70%, .7)` });
-        this.$update();
+        // if (!this.item?.[0])
+        // this.item = [{ color: `hsla(${this._dragElementColor}, 50%, 70%, .7)` }];
+        // this.$update();
     }
     _dblclick(e, color) {
-        this._dragElementColor = color;
-        this._drop();
+        // this._dragElementColor = color;
+        // this._drop();
     }
 
 });
@@ -106,9 +145,9 @@ customElements.define('li-dashpanel', class LiDashpanel extends LiElement {
                 border: 1px solid gray;
                 min-width: 200px;
                 min-height: 100px;
-                /* flex: 0 0; */
+                flex: 1 1 auto;
                 margin: 4px;
-                height: calc(50% - 10px);
+                /* height: (100% - 10px); */
                 cursor: pointer;
             }
             .focused {
@@ -119,21 +158,52 @@ customElements.define('li-dashpanel', class LiDashpanel extends LiElement {
 
     render() {
         return html`
-            <div class="panel ${!this.readOnly && this.focusedItem === this.item ? 'focused' : ''}" style="background: ${this.item?.color}" @click="${this._click}">
-
-            </div>
+            ${!this.item?.items?.[0] ? html`` : html`
+                <div style="display: flex; flex-direction: ${this.item.vertical ? 'column' : 'row'}; width: 100%; height: 100%; flex-wrap: wrap; min-width: 200px"  @drop="${this._drop}" @dragover="${this._over}"
+                        @dragstart="${this._start}">
+                    <div class="panel ${!this.readOnly && this.focusedItem === this.item.items[0] ? 'focused' : ''}" style="
+                        width: ${this.item.items[0].w || ''}; 
+                        height: ${this.item.items[0].h || ''}; 
+                        background: ${this.item.items[0].color}" @click="${this._click}"></div>
+                    ${!this.item.items[1] ? html`` : html`
+                        ${this.item.items[1].items?.length ? html`
+                            <li-dashpanel .item=${this.item.items[1]}></li-dashpanel>
+                        ` : html`
+                            <div class="panel ${!this.readOnly && this.focusedItem === this.item.items[1] ? 'focused' : ''}" style="
+                            width: ${this.item.items[0].w || ''}; 
+                            height: ${this.item.items[0].h || ''}; 
+                            background: ${this.item.items[1].color}" @click="${this._click}"></div>
+                        `}
+                    `}
+                </div>
+            `}
         `;
     }
 
     static get properties() {
         return {
-            item: { type: Object, default: undefined },
+            item: { type: Object },
             focusedItem: { type: Object, default: undefined, local: true },
-            readOnly: { type: Boolean, local: true}
+            readOnly: { type: Boolean, local: false }
         }
     }
 
     _click(e) {
         this.focusedItem = this.item;
+    }
+
+    _start(e) {
+        //console.log(color)
+        this._dragElementColor = e.target.color;
+    }
+    _over(e) {
+        //console.log(e)
+        e.preventDefault();
+        //e.dataTransfer.dropEffect = "move";
+    }
+    _drop(e) {
+        // console.log(e)
+        this.item.items[1] = { color: `hsla(45, 50%, 70%, .7)` };
+        this.$update();
     }
 });
