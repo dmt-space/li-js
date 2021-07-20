dff9ff8fa7d9bf6a066c0ec4d52767f3b4b413b2
resu <resu062@gmail.com>
2021-07-20 14:59:09 +0300

    Update dashboard.js

--- li/dashboard/dashboard.js
+++ li/dashboard/dashboard.js
@@ -107,26 +107,45 @@ customElements.define('li-dashpanel', class LiDashpanel extends LiElement {
                 box-shadow: 0 0 10px 1px orange;
                 z-index: 1;
             }
+            .marker {
+                position: absolute;
+                background: red;
+                border-radius: 5px;
+                right: 0;
+                bottom: 0;
+                width: 10px;
+                height: 10px;
+                margin: 1px;
+                z-index: 2;
+            }
         `;
     }
 
     render() {
         return html`
-            <div class="panel ${!this.readOnly && this.focusedItem === this.item ? 'focused' : ''}" 
-                style="
-                    position: absolute;
-                    background: ${this.item?.color};
-                    left: ${this.item?.left || 0 + 'px'};
-                    top: ${this.item?.top || 0 + 'px'};
-                    width: ${this.item?.w || 200 + 'px'};
-                    height: ${this.item?.h || 100 + 'px'};
-                " 
-                @click="${this._click}"
-                @mousedown="${this._down}"
-                @mousemove="${this._move}"
-                @mouseup="${this._up}"
-                @dragstart="${this._start}">
+            <div style="position: fixed; top: 0; left: 0; right: 0; bottom: 0; z-index: 1; pointer-events: ${this.action ? 'auto' : 'none'}" 
+                    ?hidden="${this.readOnly || this.focusedItem !== this.item || !this.action}"
+                    @mousemove="${this._move}" @mouseup="${this._up}">
             </div>
+            <div class="panel ${!this.readOnly && this.focusedItem === this.item ? 'focused' : ''}" 
+                    style="
+                        position: absolute;
+                        background: ${this.item?.color};
+                        left: ${this.item?.left || 0 + 'px'};
+                        top: ${this.item?.top || 0 + 'px'};
+                        width: ${this.item?.w || 200 + 'px'};
+                        height: ${this.item?.h || 100 + 'px'};
+                    " 
+                    @click="${this._click}"
+                    @mousedown="${this._down}"
+                    @mousemove="${this._move}"
+                    @mouseup="${this._up}"
+                    @dragstart="${this._start}">
+                <div class="marker" ?hidden="${this.readOnly || this.focusedItem !== this.item}"
+                        @mousedown="${this._markerDown}">
+                </div>
+            </div>
+
         `;
     }
 
@@ -146,20 +165,37 @@ customElements.define('li-dashpanel', class LiDashpanel extends LiElement {
         this.focusedItem = this.item;
     }
     _move(e) {
-        if (this.action === 'mouseMove' && this.focusedItem === this.item) {
+        if (this.focusedItem !== this.item) return;
+        if (this.action === 'mouseMove') {
             this.item.left += e.movementX;
             this.item.top += e.movementY;
             this.$update();
         }
+        if (this.action === 'markerMove') {
+            this.item.w += e.movementX;
+            this.item.h += e.movementY;
+            this.$update();
+        }
     }
     _down(e) {
-        this.action = 'mouseMove';
         this.focusedItem = this.item;
+        this.action = 'mouseMove';
     }
     _up(e) {
         e => this.action = '';
-        this.item.left = Math.round(this.item.left / 5) * 5;
-        this.item.top = Math.round(this.item.top / 5) * 5;
+        this.item.left = Math.round(this.item.left / 10) * 10;
+        this.item.top = Math.round(this.item.top / 10) * 10;
+        this.item.w = Math.round(this.item.w / 10) * 10;
+        this.item.h = Math.round(this.item.h / 10) * 10;
+        this.item.left = this.item.left < 0 ? 0 : this.item.left;
+        this.item.top = this.item.top < 0 ? 0 : this.item.top;
+        this.item.w = this.item.w < 12 ? 12 : this.item.w;
+        this.item.h = this.item.h < 12 ? 12 : this.item.h;
         this.$update();
+    }
+    _markerDown(e) {
+        e.stopPropagation();
+        this.focusedItem = this.item;
+        this.action = 'markerMove';
     }
 });
