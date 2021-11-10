8318300d454bc6f78c503986935a507e77eb2bac
resu <resu062@gmail.com>
2021-11-10 08:08:12 +0300

    Update dma.js

--- li/dma/dma.js
+++ li/dma/dma.js
@@ -33,22 +33,22 @@ customElements.define('li-dma', class LiDma extends LiElement {
                 <label style="font-weight: 600;">Входные данные:</label>
                 <div style="display: flex; flex-direction: column; flex: 1; border: 1px solid lightgray; margin: 2px;  min-width: 240px">
                     <label>Внутренний диаметр (дюйм):</label>
-                    <input id="d1" .value=${this.d1} type="number" @change=${this._input}>
+                    <input id="d1" .value=${this._d1} type="number" @change=${this._input} @blur=${this._input}>
                     <label>Внешний диаметр (дюйм):</label>
-                    <input id="d2" .value=${this.d2} type="number" @change=${this._input}>
+                    <input id="d2" .value=${this._d2} type="number" @change=${this._input} @blur=${this._input}>
                     <label>Внутренний диаметр (${this.edIzm}):</label>
-                    <input id="d3" .value=${this.d3} type="number" @change=${this._input}>
+                    <input id="d3" .value=${this._d3} type="number" @change=${this._input} @blur=${this._input}>
                     <label>Внешний диаметр (${this.edIzm}):</label>
-                    <input id="d4" .value=${this.d4} type="number" @change=${this._input}>
+                    <input id="d4" .value=${this._d4} type="number" @change=${this._input} @blur=${this._input}>
                     <div style="display: flex; align-items: center;">
                         <li-checkbox title="мм" .toggled=${this.edIzm === 'мм'} @change=${this._checked}></li-checkbox>мм
                         <li-checkbox title="см" .toggled=${this.edIzm === 'см'} @change=${this._checked}></li-checkbox>см
                     </div>
                     <div style="display: flex; align-items: center;">
-                        <li-checkbox id="ratio" @change=${this._checked}></li-checkbox>Внешний / внутренний = 3.326
+                        <li-checkbox id="ratio" .toggled=${this.ratio} @change=${this._checked}></li-checkbox>Внешний / внутренний = 3.326
                     </div>
                     <label>Диаметр провода с изоляцией (мм):</label>
-                    <input id="d0" .value=${this.d0} type="number" @change=${this._input}>
+                    <input id="d0" .value=${this.d0} type="number" @change=${this._input} @blur=${this._input}>
                     <label>Марка провода:</label>
                     <input .value=${this.type} @change=${e => this.type = e.target.value}>
                 </div>
@@ -57,9 +57,9 @@ customElements.define('li-dma', class LiDma extends LiElement {
                 <label style="font-weight: 600;">Расчетные данные:</label>
                 <div style="display: flex; flex-direction: column; flex: 1; border: 1px solid lightgray; margin: 2px; min-width: 240px">
                     <label>Количество витков (пара):</label>
-                    <input>
+                    <input .value=${this._turn} readonly>
                     <label>Длина 1-го провода (м):</label>
-                    <input>
+                    <input .value=${this._length} readonly>
                 </div>
                 <label style="font-weight: 600;">Результаты измерений:</label>
                 <div style="display: flex; flex-direction: column; flex: 1; border: 1px solid lightgray; margin: 2px; min-width: 240px">
@@ -81,9 +81,10 @@ customElements.define('li-dma', class LiDma extends LiElement {
             d3: { type: Number, default: 25.4, save: true },
             d4: { type: Number, default: 228.6, save: true },
             d0: { type: Number, default: 1, save: true },
+            turn: { type: Number, default: 0, save: true },
             edIzm: { type: String, default: 'мм', save: true },
             type: { type: String, default: '', save: true },
-            useRatio: { type: Boolean, default: false, save: true },
+            ratio: { type: Boolean, default: false, save: true },
             author: { type: String, default: 'user', save: true },
         }
     }
@@ -100,7 +101,30 @@ customElements.define('li-dma', class LiDma extends LiElement {
                 { label: 'col-004', name: 'c4' },
                 { label: 'col-005', name: 'c5', width: 120 },
                 { label: '', name: '' },
-            ]        }
+            ]
+        }
+    }
+    get _d1() {
+        return Number(this.d1).toFixed(3);
+    }
+    get _d2() {
+        return Number(this.d2).toFixed(3);
+    }
+    get _d3() {
+        return (Number(this.d3 / (this.edIzm === 'мм' ? 1 : 10))).toFixed(2);
+    }
+    get _d4() {
+        return (Number(this.d4 / (this.edIzm === 'мм' ? 1 : 10)).toFixed(2));
+    }
+    get _turn() {
+        return Number((this.d4 - this.d3) / 4 / (this.d0 || 1)).toFixed(2);
+    }
+    get _length() {
+        const pi = 3.14159265359;
+        let l = 0;
+        for (let i = 0; i < this._turn; i += this.d0) {
+
+        }
     }
 
     connectedCallback() {
@@ -118,11 +142,72 @@ customElements.define('li-dma', class LiDma extends LiElement {
             this.edIzm = e.target.title;
             e.target.toggled = true;
         } else if (e.target.id === 'ratio') {
-            this.useRatio = e.target.toggled;
+            this.ratio = e.target.toggled;
+            if (this.ratio) {
+                if (this.d2 > 0) {
+                    this.d1 = this.d2 / 3.326
+                    this.$id('d1').value = this._d1;
+                    this._input('', 'd1', this.d1);
+                } else if (this.d1 > 0) {
+                    this.d2 = this.d1 * 3.326
+                    this.$id('d2').value = this._d2;
+                    this._input('', 'd2', this.d2);
+                }
+            }
         }
         this.$update();
     }
-    _input(e) {
-        console.log(e.target.id)
+    _input(e, id, val) {
+        id = id || e?.target?.id;
+        val = val || e?.target?.value;
+        switch (id) {
+            case 'd0':
+                this.d0 = val;
+                break;
+            case 'd1':
+                this.d1 = val;
+                this.d3 = val * 25.4;
+                this.$id(id).value = this._d1;
+                if (this.ratio && !this._step) {
+                    this._step = true;
+                    this.d2 = this.d1 * 3.326;
+                    this._input('', 'd2', this.d2);
+                }
+                break;
+            case 'd2':
+                this.d2 = val;
+                this.d4 = val * 25.4;
+                this.$id(id).value = this._d2;
+                if (this.ratio && !this._step) {
+                    this._step = true;
+                    this.d1 = this.d2 / 3.326;
+                    this._input('', 'd1', this.d1);
+                }
+                break;
+            case 'd3':
+                this.d3 = val * (this.edIzm === 'мм' ? 1 : 10);
+                this.d1 = this.d3 / 25.4;
+                this.$id(id).value = this._d3;
+                if (this.ratio && !this._step) {
+                    this._step = true;
+                    this.d4 = this.d3 * 3.326
+                    this._input('', 'd4', this.d4);
+                }
+                break;
+            case 'd4':
+                this.d4 = val * (this.edIzm === 'мм' ? 1 : 10);
+                this.d2 = this.d4 / 25.4;
+                this.$id(id).value = this._d4;
+                if (this.ratio && !this._step) {
+                    this._step = true;
+                    this.d3 = this.d4 / 3.326;
+                    this._input('', 'd3', this.d3);
+                }
+                break;
+            default:
+                break;
+        }
+        this._step = false;
+        this.$update();
     }
 });
