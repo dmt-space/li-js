ca8b682fe095a4331e171ecabee959ae00b70d34
resu <resu062@gmail.com>
2021-11-21 17:45:53 +0300

    li-base

--- li/base/lib/base-cls.js
+++ li/base/lib/base-cls.js
@@ -11,24 +11,31 @@ export class ChangesMap {
     get name() { return this.db?.name }
     has(item) { return this.#map.has(item._id) }
     get(item) { return this.#map.get(item._id) }
-    set(item) { this.#map.set(item._id, item.doc) }
+    set(item) { this.#map.set(item._id, item) }
     delete(item) { this.#map.delete(item._id) }
     clear() { this.#map = new Map() }
     async save() {
         if (this.#map.size) {
+            const map = new Map();
             const res = [];
-            this.map.forEach(i => res.push(i));
+            this.map.forEach(i => map.set(i._id, i.doc));
+            const saves = await this.db.allDocs({ keys: [...map.keys()], include_docs: true });
+            saves.rows.map(i => {
+                let doc = map.get(i.key);
+                if (i.doc) doc = {...i.doc, ...doc};
+                if (i.value?.rev) doc._rev = i.value.rev;
+                res.push(doc);
+            })
             await this.db.bulkDocs(res);
-
+            console.log('save - ', this.#map.size);
             this.clear();
-            console.log('save')
         }
     }
 }
 
 export class LIITEM {
     #doc = LI.icaro({});
-    #hasChanged = false;
+    hasChanged = false;
     #fnListen = (e) => {
         const changes = [];
         e?.forEach(i => changes.push({ [i]: this.doc[i] }));
@@ -43,25 +50,17 @@ export class LIITEM {
             this.changesMap.set(this);
             console.log('..... this.changesMap: ', this.changesMap);
         }
-        this.#hasChanged = true;
+        this.hasChanged = true;
     }
-    constructor(db, item, doc) {
+    constructor(db, parent, doc, isLoad = false) {
         if (db) {
             this.$db = db;
-            this.dbLocal = db.local;
+            this.localDB = db.localDB;
             this.changesMap = db.changesMap;
-            if (item) {
-                item.items ||= [];
-                item.items.push(this);
-                item.expanded = true;
-            } else {
-                db.items ||= [];
-                db.items.push(this);
-            }
         }
         if (doc) {
             this.$doc = doc;
-            this.#doc = LI.icaro({ ...this.#doc, ...doc });
+            this.doc = LI.icaro({ ...this.doc, ...doc });
         }
         this.doc.ulid ??= LI.ulid();
         this.doc.type ??= 'li-item';
@@ -71,32 +70,40 @@ export class LIITEM {
             let ds = LI.dates(LI.ulidToDateTime(this.doc.ulid));
             this.doc.created = { utc: ds.utc, local: ds.local };
         }
-        this.#doc.listen(this.#fnListen);
+        if (parent) {
+            this.$parent = parent;
+            parent.doc.items ||= [];
+            parent.doc.items.add(this._id);
+            parent.items ||= [];
+            parent.items.add(this);
+            parent.expanded = true;
+            if (!isLoad)
+                parent.changed();
+        }
+        this.doc.listen(this.#fnListen);
     }
     get doc() { return this.#doc }
     set doc(v) { this.#doc = v }
-    get _id() { return this.#doc._id }
-    get _ref() { return this.#doc._ref }
-    get ulid() { return this.#doc.ulid }
-    get type() { return this.#doc.type }
-    get created() { return this.#doc.created }
-    get date() { return this.#doc.created.local }
-    get utc() { return this.#doc.created.utc }
-    get hasChanged() { return this.#hasChanged }
-    get label() { return this.#doc.label }
-    set label(v) { this.#doc.label = v }
-    get items() { return this.#doc.items }
-    set items(v) { this.#doc.items ||= []; this.#doc.items.push(v) }
-    get dbName() { return this.dbLocal?.name }
+    get _id() { return this.doc._id }
+    get _ref() { return this.doc._ref }
+    get ulid() { return this.doc.ulid }
+    get type() { return this.doc.type }
+    get created() { return this.doc.created }
+    get date() { return this.doc.created.local }
+    get utc() { return this.doc.created.utc }
+    //get hasChanged() { return this.#hasChanged }
+    get label() { return this.doc.label }
+    set label(v) { this.doc.label = v }
+    get dbName() { return this.localDB?.name }
     clearChanges() {
-        this.#hasChanged = false;
+        this.hasChanged = false;
         if (this.changesMap) {
             this.changesMap.delete(this);
             console.log('..... delete from Map: ', this._id);
         }
     }
 
-    async save(db = this.dbLocal) {
+    async save(db = this.localDB) {
         db.get(this.doc._id).then(doc => {
             doc = { ...doc, ...this.doc };
             return db.put(doc);
@@ -116,7 +123,7 @@ export class LIITEM {
             });
         });
     }
-    load(db = this.dbLocal) {
+    load(db = this.localDB) {
         db.get(this._id).then(doc => {
             console.log('..... load ok: ', doc);
             return this.doc;
