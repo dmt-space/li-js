81773570e1c4709ac376b00d36cc74f8d7c703a1
resu062 <you@example.comresu062@gmail.com>
2022-03-16 09:53:32 +0300

    flips

--- li/flips/flips.js
+++ li/flips/flips.js
@@ -232,7 +232,7 @@ customElements.define('li-flips', class LiFlips extends LiElement {
         const rusAlphabet = ['А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'];
         const digital1_9 = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
         const images = [];
-        const url = LI.$url.replace('li.js', 'lib/media/cards/_cards-');
+        const url = LI.$url.replace('li.js', 'li/flips/cards/_cards-');
         for (let i = 1; i <= 140; i++) {
             images.push(url + (i < 10 ? '00' + i : i < 100 ? '0' + i : i) + '.jpg'); 
         }
